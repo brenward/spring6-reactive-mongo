5011f601e9be5c4e4abf34320215058aead217e4
brenward <bren.ward@gmail.com>
2025-05-17 07:32:49 +0100

    first commit

---
+++ src/test/java/com/bwardweb/spring6_reactive_mongo/Spring6ReactiveMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwardweb.spring6_reactive_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
