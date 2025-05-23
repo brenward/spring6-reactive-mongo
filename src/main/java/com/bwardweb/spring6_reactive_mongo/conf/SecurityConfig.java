741f45f446356c7612b48214b4ccd16f59bf9494
brenward <bren.ward@gmail.com>
2025-05-23 15:35:43 +0100

    #Configuring to use oath2

---
+++ src/main/java/com/bwardweb/spring6_reactive_mongo/conf/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.bwardweb.spring6_reactive_mongo.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> {
+                    oAuth2ResourceServerSpec.jwt(Customizer.withDefaults());
+                });
+
+        return http.build();
+    }
+
+}
